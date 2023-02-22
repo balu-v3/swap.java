f3701efdd7273603fa18eda0fc46ade6b088bf23
balu-v3 <125967427+balu-v3@users.noreply.github.com>
2023-02-22 11:37:48 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,20 @@
+class Swap
+ {
+  public static void main(String args[])
+   
+  {
+   
+   int num1=3000;
+   int num2=5000;
+   
+   num1=num1+num2;
+   num2=num1-num2;
+   num1=num1-num2;
+   
+  System.out.println(num1);
+  System.out.println(num2);
+
+}
+
+}
+
